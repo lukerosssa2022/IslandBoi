301433c671f3044016a8e22d1902afebb5ee9f73
ofek shtrit <ofek1997shtrit@gmail.com>
2022-02-23 19:10:39 +0200

    implemented the ability for user to add photos

--- app/models/island.rb
+++ app/models/island.rb
@@ -6,6 +6,11 @@ class Island < ApplicationRecord
 
   COUNTRIES = ["Isreal", "Spain", "Germany", "South Africa"]
 
+  PHOTOS = ["https://www.treehugger.com/thmb/c8YBoBZfJLKUPGlFaQ7RT7J6xNA=/3840x2160/filters:fill(auto,1)/GettyImages-1296713537-aec94fc7c0154ed5812d2017e856f8d7.jpg",
+            "https://cdn.cnn.com/cnnnext/dam/assets/180219103122-zanzibar-and-its-islands---mnemba-a-view-from-the-sky-mnemba-island-lodge.jpg",
+            "https://www.treehugger.com/thmb/c8YBoBZfJLKUPGlFaQ7RT7J6xNA=/3840x2160/filters:fill(auto,1)/GettyImages-1296713537-aec94fc7c0154ed5812d2017e856f8d7.jpg",
+            "https://cdn.cnn.com/cnnnext/dam/assets/180219103122-zanzibar-and-its-islands---mnemba-a-view-from-the-sky-mnemba-island-lodge.jpg"]
+
   validates :name, :territory, :price_per_day, :size, presence: true
   include PgSearch::Model
 
